33b2bf590096e31d137f9b82ba7f27270a960b54
CommonSP <Evgeniy2000sa@gmail.com>
2023-09-20 05:04:37 +0300

    ff

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,58 +1,1 @@
-import {
-    Controller, Delete,
-    Get,
-    HttpStatus,
-    Param,
-    Post, Query,
-    Res,
-    StreamableFile,
-    UploadedFile,
-    UploadedFiles,
-    UseInterceptors
-} from '@nestjs/common'
-import {FileInterceptor, FilesInterceptor} from '@nestjs/platform-express'
-import {Response} from 'express'
-import * as fs from 'fs'
-import {diskStorage} from 'multer'
-import {extname, join} from 'path'
-import * as process from 'process'
-import {ImageService} from "./services/image.service";
-
-@Controller('images')
-export class ImageController {
-    constructor(private imageService: ImageService) {
-    }
-
-    @Post()
-    @UseInterceptors(
-        FileInterceptor('file', {
-            storage: diskStorage({
-                destination: (req, file, callback) => {
-                    const path = `../images/products/${req.query.id}`
-                    if (!fs.existsSync(path)) {
-                        fs.mkdirSync(path, {recursive: true})
-                    }
-                    callback(null, path)
-                },
-                filename: (req, file, callback) => {
-                    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-                    const ext = extname(file.originalname)
-                    callback(null, `${file.originalname}-${uniqueSuffix}${ext}`)
-                },
-            }),
-        }),
-    )
-    async saveProductImage(@Query('category') category: string, @Query('id') id: string, @UploadedFile() file: Express.Multer.File) {
-        return await this.imageService.saveImage(file.filename, id, category)
-    }
-
-    @Get('product/:id/:name')
-    async getProductImage(@Param() params: { id: string; name: string }) {
-        const file = fs.createReadStream(join(process.cwd(), '../images/products/' + params.id + '/' + params.name))
-        return new StreamableFile(file)
-    }
-    @Delete()
-    async deleteProductImage(){
-
-    }
-}
+import { Controller, Delete, Get, Param, Post, Query, StreamableFile, UploadedFile, UseInterceptors } from '@nestjs/common'import { FileInterceptor } from '@nestjs/platform-express'import * as fs from 'fs'import { diskStorage } from 'multer'import { extname, join } from 'path'import * as process from 'process'import { ImageService } from './services/image.service'@Controller('images')export class ImageController {	constructor(private imageService: ImageService) {}	@Post()	@UseInterceptors(		FileInterceptor('file', {			storage: diskStorage({				destination: (req, file, callback) => {					const path = `../images/products/${req.query.id}`					if (!fs.existsSync(path)) {						fs.mkdirSync(path, { recursive: true })					}					callback(null, path)				},				filename: (req, file, callback) => {					const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)					const ext = extname(file.originalname)					callback(null, `${file.originalname}-${uniqueSuffix}${ext}`)				},			}),		}),	)	async saveProductImage(@Query('category') category: string, @Query('id') id: string, @UploadedFile() file: Express.Multer.File) {		return await this.imageService.saveImage(file.filename, id, category)	}	@Get('product/:id/:name')	async getProductImage(@Param() params: { id: string; name: string }) {		const file = fs.createReadStream(join(process.cwd(), '../images/products/' + params.id + '/' + params.name))		return new StreamableFile(file)	}	@Post('product-index')	async indexing() {		const directories: Array<{ d: string; f: string }> = []		fs.readdirSync(join(process.cwd(), '../images/products/'), { withFileTypes: true })			.filter((d) => d.isDirectory())			.map(async (d) => {				fs.readdirSync(join(process.cwd(), '../images/products/', d.name), { withFileTypes: true }).map(async (f) => {					directories.push({ f: f.name, d: d.name })				})			})		for (const el of directories) {			console.log(el.f)			await this.imageService.saveImage(el.f, el.d, 'main')		}	}	@Delete()	async deleteProductImage() {}}
--- src/image/services/image.service.ts
+++ src/image/services/image.service.ts
@@ -1,44 +1,44 @@
-import {HttpException, HttpServer, Injectable} from "@nestjs/common";
-import {InjectRepository} from "@nestjs/typeorm";
-import {Image} from "../image.entity";
-import {Repository} from "typeorm";
-import {ProductService} from "../../products/product.service";
-import * as fs from 'fs';
-import * as path from 'path';
-import * as process from "process";
+import { HttpException, Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as process from 'process'
+import { Repository } from 'typeorm'
+import { ProductService } from '../../products/product.service'
+import { Image } from '../image.entity'
 
 @Injectable()
 export class ImageService {
-    constructor(@InjectRepository(Image) private imageRepository: Repository<Image>,
-                private productService: ProductService) {
-    }
+	constructor(@InjectRepository(Image) private imageRepository: Repository<Image>, private productService: ProductService) {}
 
-    async saveImage(name: string, guid: string, category: string) {
-        const product = await this.productService.getProductById(guid)
-        if (product) {
-            if (category == 'main') {
-                const mainImage = await this.imageRepository.findOneBy({product: product, category: 'main'})
-                if (mainImage) {
-                    mainImage.name = name
-                    return this.imageRepository.save(mainImage)
-                }
-            }
-            const image = new Image()
-            image.name = name
-            image.product = product
-            image.category = category
+	async saveImage(name: string, guid: string, category: string) {
+		const product = await this.productService.getProductById(guid)
 
-            return this.imageRepository.save(image)
-        }
-    }
+		if (product) {
+			if (category == 'main') {
+				const mainImage = await this.imageRepository.findOneBy({ product: product, category: 'main' })
+				if (mainImage) {
+					mainImage.name = name
+					console.log('done1')
+					return this.imageRepository.save(mainImage)
+				}
+			}
+			const image = new Image()
+			image.id = new Date().getMilliseconds().toString()
+			image.name = name
+			image.product = product
+			image.category = category
 
-    async deleteImage(id: string) {
-        const image = await this.imageRepository.findOneBy({id})
-        if (image) {
-            fs.unlink(path.join(process.cwd(), '../image/products', image.product.guid, image.name), (err) => {
-                if (err)
-                    throw new HttpException('Файл не найден', 500)
-            })
-        }
-    }
+			return this.imageRepository.save(image)
+		}
+	}
+
+	async deleteImage(id: string) {
+		const image = await this.imageRepository.findOneBy({ id })
+		if (image) {
+			fs.unlink(path.join(process.cwd(), '../image/products', image.product.guid, image.name), (err) => {
+				if (err) throw new HttpException('Файл не найден', 500)
+			})
+		}
+	}
 }
